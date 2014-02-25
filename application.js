68c8e3a2afb8358c3b00a22db95409b13858379c
Sumihiko Natsu <hottestseason@gmail.com>
2014-02-25 10:04:51 +0900

    Invader

--- application.js
+++ application.js
@@ -1,4 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var gmail = ['hottestseason', '@', 'gmail.com'].join('');
-  document.querySelector("#gmail").innerHTML = '<a href="mailto:' + gmail + '">' + gmail + '</a>';
+$(function() {
+    var gmail = ['hottestseason', '@', 'gmail.com'].join('');
+    $("#gmail").html('<a href="mailto:' + gmail + '">' + gmail + '</a>');
+
+    var $invaderCanon = $(".invader-canon");
+    var $brand = $(".brand");
+    var brandText = $brand.text();
+    var newBrand = "";
+    for (var i = 0; i < brandText.length; i++) {
+        newBrand += "<span>" + brandText.charAt(i) + "</span>";
+    }
+    $brand.html(newBrand);
+    $invaderCanon.click(function() {
+        var $invaderLaser = $(".invader-laser").clone().appendTo($("#navbar"));
+        var left = $invaderCanon.position().left;
+        $invaderLaser.css({ left: left });
+        var timer = setInterval(function() {
+            left -= 6;
+            var $lastChar = $(".brand > *:visible").last();
+            if ($lastChar.length > 0 && left < $lastChar.position().left + $lastChar.width()) {
+                clearInterval(timer);
+                $invaderLaser.remove();
+                $lastChar.hide();
+                if ($(".brand > *:visible").length == 0) {
+                    $(".brand > *:hidden").show();
+                }
+            } else if (left < 0) {
+                clearInterval(timer);
+            } else {
+                $invaderLaser.css({ left: left });
+            }
+        }, 25);
+    });
 });

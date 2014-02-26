0ae75ed616c72194b41c3e9465ac327aa05bf344
Sumihiko Natsu <hottestseason@gmail.com>
2014-02-27 03:45:30 +0900

    Refactor

--- application.js
+++ application.js
@@ -12,9 +12,9 @@ $(function() {
     $brand.html(newBrand);
 
     setInterval(function() {
-        $(".invader-laser.launched").each(function() {
+        $(".invader-laser").each(function() {
             $laser = $(this);
-            $laser.css({ left: "-=6" });
+            $laser.css({ left: "-=2" });
             var $lastChar = $(".brand > *:visible").last();
             if ($lastChar.length > 0 && $laser.position().left < $lastChar.position().left + $lastChar.width()) {
                 $laser.remove();
@@ -24,18 +24,12 @@ $(function() {
                 }
             }
         });
-    }, 20);
+    }, 5);
 
-    $invaderCanon.click(function(event) {
-        $(".invader-laser:not(.launched)").clone().addClass("launched").appendTo($("#navbar")).css({ left: $invaderCanon.position().left });
-    });
+    $invaderCanon.click(function(event) { $("<div class='invader-laser'></div>").appendTo($("#navbar")).css({ left: $invaderCanon.position().left }); });
 
-    setInterval(function() {
-        $invaderCanon.click();
-        setTimeout(function() {
-            $(".brand > *:hidden").first().show();
-        }, Math.random() * 7000);
-    }, 3000);
+    (fireLaser = function() { $invaderCanon.click(); setTimeout(fireLaser, Math.random() * 3000); })();
+    (recoverBrandChar = function() { $(".brand > *:hidden").first().show(); setTimeout(recoverBrandChar, Math.random() * 4000); })();
 
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments);},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
     ga('create', 'UA-48457330-1', 'hottestseason.com');
